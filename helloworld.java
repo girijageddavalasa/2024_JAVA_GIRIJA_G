dc42d3faf8c3b5855c62d7647af8aa6bd9dd9de9
girijageddavalasa <148637003+girijageddavalasa@users.noreply.github.com>
2024-07-23 18:54:03 +0530

    Hello world

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class helloworld
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello world");
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("name="+name);
+    }
+}
